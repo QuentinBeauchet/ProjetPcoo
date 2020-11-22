4d4b54934cfa2c37f62fb23b11eb6315ecd86ce4
GillianMASSE <gillian83.masse@gmail.com>
2020-11-22 15:35:26 +0100

    oui

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
+        String str ="Yann est gay";
+        boolean b=true;
+
+        if(b)
+        {
+            System.out.print(str);
+        }
+
     }
 }
