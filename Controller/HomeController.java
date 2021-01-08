d92e326699dab402e59c3d1c044475c545a991e9
Edojir <quentinbeauchet@free.fr>
2021-01-08 21:01:44 +0100

    creation data

--- Controller/HomeController.java
+++ Controller/HomeController.java
@@ -10,7 +10,7 @@ public class HomeController {
      * Classe du controller qui appele la vue de depart du programme.
      */
     public HomeController() {
-        (new File("data")).mkdir();
+        new File("data").mkdir();
         new StartView();
     }
 
